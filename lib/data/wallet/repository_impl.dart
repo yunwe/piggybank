6676554b2c41078435f3039c8a98728562c026b3
yunwe <sawyunwe.ucsy@gmail.com>
2024-05-23 14:54:13 +0700

    created wallet repository

---
+++ lib/data/wallet/repository_impl.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:piggybank/domain/model/wallet.dart';
+import 'package:piggybank/domain/repository/wallet_repository.dart';
+
+class FirebaseWalletRepository implements WalletRepository {
+  FirebaseWalletRepository({
+    FirebaseFirestore? db,
+  }) : _db = db ?? FirebaseFirestore.instance;
+
+  final FirebaseFirestore _db;
+
+  String collectionPath(String userId) => 'wallets/$userId';
+
+  @override
+  Future<void> create({
+    required String userId,
+    required String title,
+    double? targetAmount,
+    DateTime? targetEndDate,
+  }) async {
+    final wallet = <String, dynamic>{
+      'title': title,
+      'amount': 0,
+      'target_amount': targetAmount,
+      'target_date': targetEndDate,
+      'created_at': DateTime.now(),
+      'is_archived': false,
+      'archived_at': null,
+      'is_deleted': false,
+      'deleted_at': null,
+    };
+
+    _db.collection(collectionPath(userId)).add(wallet).then(
+          (DocumentReference doc) => print('DocumentSnapshot added with ID: ${doc.id}'),
+        );
+  }
+
+  @override
+  Future<void> delete({required String id}) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Wallet> get({required String id}) {
+    // TODO: implement get
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Wallet>> getAll() {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> update() {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+}
---
+++ lib/domain/repository/wallet_repository.dart
@@ -0,0 +1,19 @@
+import '../model/models.dart';
+
+abstract class WalletRepository {
+  //Input Functions
+  Future<void> create({
+    required String userId,
+    required String title,
+    double? targetAmount,
+    DateTime? targetEndDate,
+  });
+
+  Future<void> update();
+
+  Future<void> delete({required String id});
+
+  Future<List<Wallet>> getAll();
+
+  Future<Wallet> get({required String id});
+}
